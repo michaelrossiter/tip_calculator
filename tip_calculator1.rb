bed051fc94036c80696ce4dbd83460808b67a85b
Michael Rossiter <michael.b.s.rossiter@gmail.com>
2014-09-11 12:57:04 -0700

    adding final tip calculator 1

--- tip_calculator1.rb
+++ tip_calculator1.rb
@@ -1,10 +1,16 @@
+# Collect basic values from user
 meal = 20
 tax_rate = 0.0625
+tip_rate = 0.2
+
+# calculations
 tax_value = meal * tax_rate
 meal_with_tax = meal + tax_value
-tip_rate = 0.2
 tip_value = meal * tip_rate
 total_cost = meal_with_tax + tip_value
+
+
+# display stuff back to user
 
 puts "The cost of the meal is: $#{meal}"
 puts "The cost of tax is: $#{tax_value}"
